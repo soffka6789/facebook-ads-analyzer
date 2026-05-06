46214a6c104291d39c40f0343d3498334dd5925d
soffka6789 <shrksf@ukr.net>
2026-05-07 00:23:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 import os
+import asyncio
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "8686753284:AAGOAIBCoWSmno-z6T635MHCyWWceZ3z_m4")
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "TOKEN")
 USER_ID = int(os.getenv("USER_ID", "5989342315"))
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -12,12 +13,13 @@ async def report(update: Update, context: ContextTypes.DEFAULT_TYPE):
     msg = "TOP-10 ТОВАРІВ:\n1. Портативна лампа +18%\n2. Еко-пляшка +45%"
     await update.message.reply_text(msg)
 
-def main():
+async def main():
     app = Application.builder().token(TELEGRAM_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("report", report))
+
     print("✅ БОТ ЗАПУЩЕН!")
-    app.run_polling()
+    await app.run_polling()
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
